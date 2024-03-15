6a9ed3dd470a76382eea4f91806e73749c8a804e
Hardcodepunk <howl@hardcodepunk.be>
2024-03-16 00:59:29 +0100

    implement blender file

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 import * as THREE from 'three';
+import Lenis from '@studio-freight/lenis'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+
+    antialias: true,
+});
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+camera.position.set(0,1,2)
 
-camera.position.z = 5;
+const mainLight = new THREE.PointLight('white', 10)
+mainLight.position.set(5,5,0)
 
-function display() {
-	
+const secondLight = new THREE.PointLight('white', 10)
+secondLight.position.set(-5,5,0)
+
+const ambientLight = new THREE.AmbientLight('white', 0.5)
+
+scene.add(mainLight,secondLight,ambientLight)
+
+
+const loader = new GLTFLoader();
+
+loader.load( '/female-base--mixamo--animation--v1.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+});
+
+function animate() {
+	requestAnimationFrame( animate );
 
 	renderer.render( scene, camera );
 }
 
-display();
+animate();
