f5e9ff63935ee5d3f602531c4beff3ec22c65a5b
Hardcodepunk <howl@hardcodepunk.be>
2024-11-08 00:17:24 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    // This ensures Vite serves `index.html` for unknown routes, letting the client-side router handle them
-    historyApiFallback: true
-  }
+  base: '/',
 });
 
